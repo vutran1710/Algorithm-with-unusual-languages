7e19e475b577f92a0f6a48241971d98253052ae7
Vu Tran <vutran@Vus-iMac.local>
2021-09-04 00:34:30 +0700

    draft

--- clojure/src/clj_algo/core.clj
+++ clojure/src/clj_algo/core.clj
@@ -1,4 +1,6 @@
 (ns clj-algo.core)
+;; libs
+(defn abs [n] (max n (- n)))
 
 (defn migratoryBirds
   "Problem at https://www.hackerrank.com/challenges/migratory-birds"
@@ -17,13 +19,13 @@
   (let [divisible? (fn [x1 x2 k]
                      (zero? (mod (+ x1 x2) k)))]
     (reduce-kv
-     (fn [cnt idx num]
-       (let [sub-ar     (subvec ar (inc idx))
-             matches    (filter #(divisible? num % k) sub-ar)
-             should-inc (count matches)]
-         (+ cnt should-inc)))
-     0
-     ar)))
+      (fn [cnt idx num]
+        (let [sub-ar     (subvec ar (inc idx))
+              matches    (filter #(divisible? num % k) sub-ar)
+              should-inc (count matches)]
+          (+ cnt should-inc)))
+      0
+      ar)))
 
 (defn bonAppetit
   "https://www.hackerrank.com/challenges/bon-appetit/problem"
@@ -34,3 +36,27 @@
         dif        (- b anna-share)
         output     (if (zero? dif) "Bon Appetit" dif)]
     (println output)))
+
+(defn pickingNumbers
+  "https://www.hackerrank.com/challenges/picking-numbers/problem"
+  [a]
+
+  (let [occur (frequencies a)
+        keyes (sort < (keys occur))
+        idxes (range (dec (count keyes)))]
+    (if (= 1 (count keyes))
+      (first (vals occur))
+      (reduce
+        (fn [res i]
+          (let [k1  (nth keyes i)
+                k2  (nth keyes (inc i))
+                v1  (get occur k1)
+                v2  (get occur k2)
+                cnt (+ v1 v2)]
+            (println res i ">>>" k1 k2 v1 v2 cnt)
+            (if (and
+                  (>= 1 (abs (- k1 k2)))
+                  (> cnt res))
+              cnt res)))
+        0
+        idxes))))
