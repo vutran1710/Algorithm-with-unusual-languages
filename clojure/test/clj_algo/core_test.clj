d55561021baed2cec520b1df03bb5e1a535dd493
Vu Tran <vutran@Vus-iMac.local>
2021-09-06 18:21:44 +0700

    add test

--- clojure/test/clj_algo/core_test.clj
+++ clojure/test/clj_algo/core_test.clj
@@ -20,3 +20,13 @@
   (is (= (pickingNumbers [1 1 1]) 3))
   (is (= (pickingNumbers [1 1 1 3]) 3))
   (is (= (pickingNumbers [1 2 2 3 1 2]) 5)))
+
+(deftest test-saveThePrisoner
+  (is (= (saveThePrisoner 4 6 2) 3))
+  (is (= (saveThePrisoner 5 2 1) 2))
+  (is (= (saveThePrisoner 5 2 2) 3))
+  (is (= (saveThePrisoner 7 19 2) 6))
+  (is (= (saveThePrisoner 3 7 3) 3))
+  (is (= (saveThePrisoner 2 576581 1) 1))
+  (is (= (saveThePrisoner 46934 543563655 46743) 20809))
+  (is (= (saveThePrisoner 3 1 2) 2)))
